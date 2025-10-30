1e3a545c53365f5dd5acd595c959f8004a945883
Manuel Cárdenas Moza <cardenasmanuelm@gmail.com>
2025-10-30 10:53:36 -0500

    migrate: add cotizacion_articulo and orden_servicio_servicio line-item entities + repositories (inferred fields)

---
+++ src/main/java/pe/nach/domain/repository/CotizacionArticuloRepository.java
@@ -0,0 +1,8 @@
+package pe.nach.domain.repository;
+
+import pe.nach.domain.entity.CotizacionArticulo;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface CotizacionArticuloRepository extends JpaRepository<CotizacionArticulo, Integer> {
+
+}
---
+++ src/main/java/pe/nach/domain/repository/OrdenServicioServicioRepository.java
@@ -0,0 +1,8 @@
+package pe.nach.domain.repository;
+
+import pe.nach.domain.entity.OrdenServicioServicio;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface OrdenServicioServicioRepository extends JpaRepository<OrdenServicioServicio, Integer> {
+
+}
